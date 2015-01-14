15f76b58354f574c936b09896ed3b690153b65d7
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-01-13 21:51:35 -0500

    [add] First commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject four-clojure "0.1.0-SNAPSHOT"
+  :description "Learn clojure through a problem set"
+  :url "http://github.com/nhunzaker/four-clojure"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:dev {:plugins [[cider/cider-nrepl "0.7.0"]]}})
